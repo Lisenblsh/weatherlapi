4aee210c7bffb8e2f96e4bcf2630e90609c56d5a
Lisenblsh <lemi013794@gmail.com>
2022-05-31 17:26:40 +0700

    convert

--- weatherApi/Controllers/WeatherController.cs
+++ weatherApi/Controllers/WeatherController.cs
@@ -128,7 +128,7 @@ namespace weatherApi.Controllers
                 Humidity = forecast.Humidity,
                 Cloudness = forecast.Cloudness,
                 ConditionEng = forecast.Condition,
-                ConditionRu = getConditionRu(forecast.Condition).ToString(),
+                ConditionRu = System.Text.Encoding.UTF8.GetString(System.Text.Encoding.UTF8.GetBytes(getConditionRu(forecast.Condition))),
                 SystemIconName = getSystemIconName(forecast.Condition),
                 UvIndex = forecast.UvIndex
             };
